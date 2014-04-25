c69dd090b9ef48966674b01b98e8067d8fe41574
gnoynait <gnoynait@gmail.com>
2014-04-26 01:55:38 +0800

    distribution

--- werewolf.js
+++ werewolf.js
@@ -1,17 +1,17 @@
 var Role = {
-	description: "",
-	createNew: function () {
+	description : "",
+	createNew : function() {
 		var r = {};
 		return r;
 	}
 };
 
 var Judge = {
-	count: 0,
-	judge: null,
-	name: "Judge",
-	createNew: function () {
-		if (Judge.judge == null) 
+	count : 0,
+	judge : null,
+	name : "Judge",
+	createNew : function() {
+		if (Judge.judge == null)
 			Judge.judge = Role.createNew();
 		Judge.judge.role = Judge;
 		return Judge.judge;
@@ -19,13 +19,13 @@ var Judge = {
 };
 
 var Player = {
-	aliveNumber: 0,
-	createNew: function () {
+	aliveNumber : 0,
+	createNew : function() {
 		var player = Role.createNew();
 		player.isAlive = true;
 		return player;
 	},
-	update: function () {
+	update : function() {
 		if (poinsoned || (killed && !guarded && !antidote)) {
 			this.die();
 		}
@@ -33,13 +33,13 @@ var Player = {
 };
 
 var Werewolf = {
-	aliveNumber: 0,
-	count: 0,
-	name: "Werewolf",
-	createNew: function () {
+	aliveNumber : 0,
+	count : 0,
+	name : "Werewolf",
+	createNew : function() {
 		var werewolf = Player.createNew();
 		werewolf.role = Werewolf;
-		werewolf.die = function () {
+		werewolf.die = function() {
 			werewolf.isAlive = false;
 			aliveNumber--;
 		};
@@ -48,11 +48,11 @@ var Werewolf = {
 };
 
 var Villager = {
-	aliveNumber: 0,
-	count: 0,
-	createNew: function () {
-		var villager = Player.createNew ();
-		villager.die = function () {
+	aliveNumber : 0,
+	count : 0,
+	createNew : function() {
+		var villager = Player.createNew();
+		villager.die = function() {
 			villager.isAlive = false;
 			aliveNumber--;
 		};
@@ -61,11 +61,11 @@ var Villager = {
 };
 
 var Miller = {
-	lovers: [0, 0],
-	miller: null,
-	count: 0,
-	name: "Miller",
-	createNew: function () {
+	lovers : [ 0, 0 ],
+	miller : null,
+	count : 0,
+	name : "Miller",
+	createNew : function() {
 		if (Miller.miller == null)
 			Miller.miller = Villager.createNew();
 		Miller.miller.role = Miller;
@@ -74,108 +74,119 @@ var Miller = {
 };
 
 var Oracle = {
-	oracle: null,
-	count: 0,
-	name: "Oracle",
-	createNew: function () {
+	oracle : null,
+	count : 0,
+	name : "Oracle",
+	createNew : function() {
 		if (Oracle.oracle == null)
-			Oracle.oracle = Villager.createNew ();
+			Oracle.oracle = Villager.createNew();
 		Oracle.oracle.role = Oracle;
 		return Oracle.oracle;
 	}
 };
 
 var Wizard = {
-	hasPoison: true,
-	hasAntidote: true,
-	wizard: null,
-	count: 0,
-	name: "Wizard",
-	createNew: function () {
+	hasPoison : true,
+	hasAntidote : true,
+	wizard : null,
+	count : 0,
+	name : "Wizard",
+	createNew : function() {
 		if (Wizard.wizard == null)
-			Wizard.wizard = Villager.createNew ();
+			Wizard.wizard = Villager.createNew();
 		Wizard.wizard.role = Wizard;
 		return Wizard.wizard;
 	}
 };
 
 var Hunter = {
-	hunter: null,
-	count: 0,
-	name: "Hunter",
-	createNew: function () {
+	hunter : null,
+	count : 0,
+	name : "Hunter",
+	createNew : function() {
 		if (Hunter.hunter == null)
-			Hunter.hunter = Villager.createNew ();
+			Hunter.hunter = Villager.createNew();
 		Hunter.hunter.role = Hunter;
 		return Hunter.hunter;
 	}
 };
 
 var Guardian = {
-	guardian: null,
-	count: 0,
-	name: "Guardian",
-	createNew: function () {
+	guardian : null,
+	count : 0,
+	name : "Guardian",
+	createNew : function() {
 		if (Guardian.guadian == null)
-			Guardian.guadian = Villager.createNew ();
+			Guardian.guadian = Villager.createNew();
 		Guardian.guadian.role = Guardian;
 		return Guardian.guadian;
 	}
 };
 
 var Elder = {
-	elder: null,
-	count: 0,
-	name: "Elder",
-	createNew: function () {
+	elder : null,
+	count : 0,
+	name : "Elder",
+	createNew : function() {
 		if (Elder.elder == null)
-			Elder.elder = Villager.createNew ();
+			Elder.elder = Villager.createNew();
 		Elder.elder.role = Elder;
 		return Elder.elder;
 	}
 };
+var Idiot = {
+	idiot : null,
+	count : 0,
+	name : "Idiot",
+	createNew : function() {
+		if (Idiot.idiot == null)
+			Idiot.idiot = Villager.createNew();
+		Idiot.idiot.role = Idiot;
+		return Idiot.idiot;
+	}
+};
+var Scapegoat = {
+	scapegoat : null,
+	count : 0,
+	name : "Scapegoat",
+	createNew : function() {
+		if (Scapegoat.scapegoat == null)
+			Scapegoat.scapegoat = Villager.createNew();
+		Scapegoat.scapegoat.role = Scapegoat;
+		return Scapegoat.scapegoat;
+	}
+};
 var NormalVillager = {
-	count: 0,
-	name: "NormalVillager",
-	createNew: function () {
-		 var villager = Villager.createNew ();
-		 villager.role = NormalVillager;
-		 return villager;
+	count : 0,
+	name : "NormalVillager",
+	createNew : function() {
+		var villager = Villager.createNew();
+		villager.role = NormalVillager;
+		return villager;
 	}
 };
 
 var Detective = {
-	detective: null,
-	transferTo: function (player) {
+	detective : null,
+	transferTo : function(player) {
 		detective = player;
 	}
 };
 
-var roles = [Judge, Werewolf, Wizard, Oracle, Guardian, Elder, NormalVillager, Hunter, Miller];
+var roles = [ Judge, Werewolf, Wizard, Oracle, Guardian, Elder, NormalVillager,
+		Hunter, Miller, Idiot, Scapegoat ];
 var people = [];
-function initRoles (n) {
+function initRoles(n) {
 	people = new Array();
-	for (var i = 0; i < roles.length; i++) {
-		for (var j = 0; j < roles[i].count; j++) {
-			people.push (roles[i].createNew ());
+	for ( var i = 0; i < roles.length; i++) {
+		for ( var j = 0; j < roles[i].count; j++) {
+			people.push(roles[i].createNew());
 		}
 	}
-	for (var i = people.length - 1; i >= 0; i--) {
-		var r =  Math.floor((Math.random () * (i + 1)));
+	for ( var i = people.length - 1; i >= 0; i--) {
+		var r = Math.floor((Math.random() * (i + 1)));
 		var t = people[i];
 		people[i] = people[r];
 		people[r] = t;
 	}
-}
-function settingDone () {
-	//alert($("#addWizard")[0].checked);
-	Werewolf.count = $("#werewolfNumber")[0].value;
-	NormalVillager.count = $("#normalNumber")[0].value;
-	Oracle.count = $("#addOracle")[0].checked;
-	Wizard.count = $("#addWizard")[0].checked;
-	Hunter.count = $("#addHunter")[0].checked;
-	Miller.count = $("#addMiller")[0].checked;
-	Guardian.count = $("#addGuardian")[0].checked;
-	Elder.count = $("#addElder")[0].checked;
-	initRoles();
 }
